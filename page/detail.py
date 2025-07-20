fea34644cc770cfc28b53b3a69659f3610493423
RemJJJ <2792674095@qq.com>
2025-07-20 15:16:33 +0800

    搜索结果页面处理

--- page/detail.py
+++ page/detail.py
@@ -1,7 +1,16 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, redirect, url_for
+from models import House
 
-detail_page = Blueprint('detail_page', __name__)
+detail_page = Blueprint("detail_page", __name__)
 
-@detail_page.route('/house')
-def index():
-    return render_template('detail.html')
+
+@detail_page.route("/house/<id>")
+def index(id=None):
+    if not id:
+        return redirect(url_for("index_page.index"))
+
+    house = House.query.filter_by(id=id).first()
+    if not house:
+        return redirect(url_for("index_page.index"))
+
+    return render_template("detail.html", house=house)
